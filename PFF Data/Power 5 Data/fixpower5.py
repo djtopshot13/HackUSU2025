b3bd83356a8f403f45cf60ada11f922bf24bd731
HaileyHal <haileyhendrickson28@gmail.com>
2025-03-01 09:02:07 -0700

    cleaned offensive grades

--- PFF Data/Power 5 Data/fixpower5.py
+++ PFF Data/Power 5 Data/fixpower5.py
@@ -1,6 +1,6 @@
 import pandas as pd
-df = pd.read_csv('Power 5 Defense Grades.csv')
+df = pd.read_csv('Power 5 Offense Grades.csv')
 
 df = df.drop(columns=['Rank', 'Unnamed: 1', 'Player GSIS', 'Player Sportradar Id'])
 
-df.to_csv('Power 5 Defense Grades.csv', index=False)
+df.to_csv('Power 5 Offense Grades.csv', index=False)
