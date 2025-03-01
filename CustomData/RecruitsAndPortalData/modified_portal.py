f27aae098ae2cf2e94da896592eb120190736b1d
HaileyHal <haileyhendrickson28@gmail.com>
2025-03-01 01:52:37 -0700

    here's all my shitty code that dillon wanted. enjoy. im going to bed

--- CustomData/RecruitsAndPortalData/modified_portal.py
+++ CustomData/RecruitsAndPortalData/modified_portal.py
@@ -4,15 +4,19 @@ NIL_df = pd.read_csv('NILData/NIL.csv')
 
 # Merge on 'school' to bring in 'collective_funding'
 merged_df = portal_df.merge(NIL_df, left_on='origin', right_on='School', how='left')
-
 # Fill missing values in 'originNIL' using 'collective_funding'
 merged_df['originNIL'] = merged_df['originNIL'].fillna(merged_df['Collective Funding'])
-
 # Drop the extra 'collective_funding' column (we've already used it)
 merged_df = merged_df.drop(columns=['Collective Funding'])
 
-#print(merged_df)
-#print(merged_df.info())
-print(merged_df['originNIL'].isnull().sum())
+# Create dictionary from NIL_df
+school_to_nil = NIL_df.set_index('School')['Collective Funding'].to_dict()
 
-merged_df.to_csv('RecruitsAndPortalData/modified_CFB_Portal_Data.csv', index=False)
+# Map NIL values directly into merged_df
+merged_df['destinationNIL'] = merged_df['destination'].map(school_to_nil)
+
+print(merged_df)
+# print(merged_df.info())
+
+# merged_df.to_csv('RecruitsAndPortalData/modified_CFB_Portal_Data.csv', index=False)
+
