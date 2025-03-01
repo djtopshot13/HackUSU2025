d529bc6862b56a8b99cd53a21aade6ba5f8dc9ea
HaileyHal <haileyhendrickson28@gmail.com>
2025-03-01 01:13:34 -0700

    added in some origin NIL numbers. work in progress

---
+++ CustomData/RecruitsAndPortalData/modified_portal.py
@@ -0,0 +1,18 @@
+import pandas as pd
+portal_df = pd.read_csv('RecruitsAndPortalData/modified_CFB_Portal_Data.csv')
+NIL_df = pd.read_csv('NILData/NIL.csv')
+
+# Merge on 'school' to bring in 'collective_funding'
+merged_df = portal_df.merge(NIL_df, left_on='origin', right_on='School', how='left')
+
+# Fill missing values in 'originNIL' using 'collective_funding'
+merged_df['originNIL'] = merged_df['originNIL'].fillna(merged_df['Collective Funding'])
+
+# Drop the extra 'collective_funding' column (we've already used it)
+merged_df = merged_df.drop(columns=['Collective Funding'])
+
+#print(merged_df)
+#print(merged_df.info())
+print(merged_df['originNIL'].isnull().sum())
+
+merged_df.to_csv('RecruitsAndPortalData/modified_CFB_Portal_Data.csv', index=False)
