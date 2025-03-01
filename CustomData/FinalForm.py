9824ee2a981b93252922790a9bb81d07d6a08174
djtopshot13 <djtopshot13@gmail.com>
2025-03-01 11:03:44 -0700

    Got the final csv set up

--- CustomData/FinalForm.py
+++ CustomData/FinalForm.py
@@ -7,9 +7,9 @@ cfb22 = pd.read_csv('CFBteam/clean_cfb22.csv')
 cfb23 = pd.read_csv('CFBteam/clean_cfb23.csv')
 
 # Calculate win percentage for each year
-cfb21['win%'] = cfb21['wins'] / (cfb21['wins'] + cfb21['losses'])
-cfb22['win%'] = cfb22['wins'] / (cfb22['wins'] + cfb22['losses'])
-cfb23['win%'] = cfb23['wins'] / (cfb23['wins'] + cfb23['losses'])
+cfb21['win%'] = round(cfb21['Win'] / cfb21['Games'], 2) * 100
+cfb22['win%'] = round(cfb22['Win'] / cfb22['Games'], 2) * 100
+cfb23['win%'] = round(cfb23['Win'] / cfb23['Games'], 2) * 100
 
 # Create a dictionary to map team names to their win percentages for each year
 win_percentages = {
@@ -19,7 +19,7 @@ win_percentages = {
 }
 
 # Load the transfer portal data
-portal_data = pd.read_csv('modified_CFB_Portal_Data.csv')
+portal_data = pd.read_csv('RecruitsAndPortalData/modified_CFB_Portal_Data.csv')
 
 # Add win percentages to the portal data
 portal_data['origin21%'] = portal_data['origin'].map(win_percentages[2021])
@@ -29,5 +29,18 @@ portal_data['destination21%'] = portal_data['destination'].map(win_percentages[2
 portal_data['destination22%'] = portal_data['destination'].map(win_percentages[2022])
 portal_data['destination23%'] = portal_data['destination'].map(win_percentages[2023])
 
+o21 = portal_data.pop('origin21%')
+o22 = portal_data.pop('origin22%')
+o23 = portal_data.pop('origin23%')
+d21 = portal_data.pop('destination21%')
+d22 = portal_data.pop('destination22%')
+d23 = portal_data.pop('destination23%')
+portal_data.insert(loc=6, column='origin21%', value=o21)
+portal_data.insert(loc=7, column='origin22%', value=o22)
+portal_data.insert(loc=8, column='origin23%', value=o23)
+portal_data.insert(loc=11, column='destination21%', value=d21)
+portal_data.insert(loc=12, column='destination22%', value=d22)
+portal_data.insert(loc=13, column='destination23%', value=d23)
+
 # Save the modified portal data to a new CSV file
-portal_data.to_csv('modified_CFB_Portal_Data.csv', index=False)
+portal_data.to_csv('finalData.csv', index=False)
